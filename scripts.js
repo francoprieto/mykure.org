8c536f655bc3c1f09d167cdf2fd7567ba276cbf5
franco <francoprieto@gmail.com>
2024-11-30 14:05:47 -0300

    Agregamos reproductor de audio

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 import { animate } from "https://cdn.jsdelivr.net/npm/framer-motion@11.11.11/dom/+esm";
-import { Howl, Howler } from "https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.4/howler.min.js";
 
 const logo = document.querySelector(".logo");
 const disk = document.querySelector(".disk");
+
+var song = new Howl({
+  src: ['./assets/mykure.mp3'],
+  html5: true,
+  loop: true,
+});
 
 animate(".disk", {scale: [0,0]});
 
@@ -16,9 +21,11 @@ animate(
 logo.addEventListener("click", ()=>{
     animate(".logo", { scale: [1, 0] }, { ease: "circInOut", duration: 1 });
     animate(".disk", { scale: [0, 1] }, { ease: "circInOut", duration: 1 });
+    song.play();
 });
 
 disk.addEventListener("click", ()=>{
     animate(".disk", { scale: [1, 0] }, { ease: "circInOut", duration: 1 });
     animate(".logo", { scale: [0, 1] }, { ease: "circInOut", duration: 1 });
+    song.pause();
 });
